683cad421426bd2dea25684bdf3dccaaaa14ff2c
Vishnuvardhanl45 <156187202+Vishnuvardhanl45@users.noreply.github.com>
2024-01-22 21:44:51 +0530

    Add files via upload

---
+++ MotionCutInternship/src/Quiz.java
@@ -0,0 +1,23 @@
+import java.util.List;
+class Quiz
+{
+    private List<Question> questions;
+    public Quiz(List<Question> questions)
+    {
+        this.questions = questions;
+    }
+
+    public Question getQuestion(int index)
+    {
+        if (index >= 0 && index < questions.size())
+        {
+            return questions.get(index);
+        }
+        return null;
+    }
+
+    public int getTotalQuestions()
+    {
+        return questions.size();
+    }
+}
